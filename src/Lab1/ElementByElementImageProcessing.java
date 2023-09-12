3811b42a35defe6b6a9df269814b9e0cb991b13d
Александр Дубровин <xah799@mail.ru>
2023-09-12 08:45:54 +0300

    Renamed ElementByElementImageProcessing interface to TransformationLogic

--- src/Lab1/ElementByElementImageProcessing.java
+++
@@ -1,5 +0,0 @@
-package Lab1;
-
-public interface ElementByElementImageProcessing {
-    void transformation();
-}
---
+++ src/Lab1/TransformationLogic.java
@@ -0,0 +1,5 @@
+package Lab1;
+
+public interface TransformationLogic {
+    void transformation();
+}
