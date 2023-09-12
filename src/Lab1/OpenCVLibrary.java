b8fa8c2e7f6e49709d5607a98f5c6b7ad5f38cec
Александр Дубровин <xah799@mail.ru>
2023-09-12 08:58:16 +0300

    Created OpenCVLibrary class and init() method for an initialization openCV library

---
+++ src/Lab1/OpenCVLibrary.java
@@ -0,0 +1,10 @@
+package Lab1;
+
+import org.opencv.core.Core;
+
+public class OpenCVLibrary {
+    public static void init() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println("Version " + Core.VERSION);
+    }
+}
